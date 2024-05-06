9423e3be7ee2c4f5833c58cc92722c25e36df381
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-06 14:40:28 +0200

    Add → added printing in page of Hotels' info

--- index.php
+++ index.php
@@ -38,6 +38,44 @@
             'distance_to_center' => 50
         ],
 
-    ];
-
+    ];    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+</head>
+<body>
+    <main>
+        <div class="hotels">
+            <?php
+            foreach($hotels as $hotel){
+
+                $hotel_name = $hotel['name'];
+                $hotel_description = $hotel['description'];
+                $hotel_has_parking = $hotel['parking'] ? 'Ha il parcheggio' : 'Non ha il parcheggio';
+                $hotel_vote = $hotel['vote'];
+                $holel_distance = $hotel['distance_to_center'];
+            
+                $hotel_text = 
+                "               
+                    <h3> Nome: $hotel_name </h3>
+                    <P> Descrizione: $hotel_description</p>
+                    <p> $hotel_has_parking </p>
+                    <p> Hotel $hotel_vote stelle </p>
+                    <p> $holel_distance km dal centro</p>
+                ";
+            ?>
+                <div class="hotel">
+                    <?php echo $hotel_text ?>
+                </div>
+            <?php        
+            };
+            ?>
+        </div>
+    </main>
+    
+</body>
+</html>
