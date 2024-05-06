3df1bc64e7da8cfdaf5d2ca198472aedfd3e8c41
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-06 14:41:49 +0200

    Add → added bootstrap to project

--- index.php
+++ index.php
@@ -46,36 +46,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <main>
-        <div class="hotels">
-            <?php
-            foreach($hotels as $hotel){
-
-                $hotel_name = $hotel['name'];
-                $hotel_description = $hotel['description'];
-                $hotel_has_parking = $hotel['parking'] ? 'Ha il parcheggio' : 'Non ha il parcheggio';
-                $hotel_vote = $hotel['vote'];
-                $holel_distance = $hotel['distance_to_center'];
-            
-                $hotel_text = 
-                "               
-                    <h3> Nome: $hotel_name </h3>
-                    <P> Descrizione: $hotel_description</p>
-                    <p> $hotel_has_parking </p>
-                    <p> Hotel $hotel_vote stelle </p>
-                    <p> $holel_distance km dal centro</p>
-                ";
-            ?>
-                <div class="hotel">
-                    <?php echo $hotel_text ?>
-                </div>
-            <?php        
-            };
-            ?>
+        <div class="container">
+            <div class="hotels">
+                <?php
+                foreach($hotels as $hotel){
+    
+                    $hotel_name = $hotel['name'];
+                    $hotel_description = $hotel['description'];
+                    $hotel_has_parking = $hotel['parking'] ? 'Ha il parcheggio' : 'Non ha il parcheggio';
+                    $hotel_vote = $hotel['vote'];
+                    $holel_distance = $hotel['distance_to_center'];
+                
+                    $hotel_text = 
+                    "               
+                        <h3> Nome: $hotel_name </h3>
+                        <P> Descrizione: $hotel_description</p>
+                        <p> $hotel_has_parking </p>
+                        <p> Hotel $hotel_vote stelle </p>
+                        <p> $holel_distance km dal centro</p>
+                    ";
+                ?>
+                    <div class="hotel">
+                        <?php echo $hotel_text ?>
+                    </div>
+                <?php        
+                };
+                ?>
+            </div>
         </div>
     </main>
     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
